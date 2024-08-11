c643a15a2dd18729fb1da8e9c476b705725bfde8
Joy Wambui <joy.wambui@student.moringaschool.com>
2024-08-12 01:51:35 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,22 @@ import About from "./About";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+import Links from "./Links";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color="firebrick" name="Liza" city="New York" />
+      <About
+        bio="I made this!"
+        github="https://github.com/Liza"
+        linkedin="https://www.linkedin.com/in/Liza/"
+      />
+      {/* <Links
+        github="https://github.com/liza"
+        linkedin="https://www.linkedin.com/in/liza/"
+      /> */}
     </div>
   );
 }
